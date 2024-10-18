34e9867a4f238865df691ce95052c4c6b8fd5d48
M9-GBR <150633267+M9-GBR@users.noreply.github.com>
2024-10-18 01:19:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let data = {
-    pp: {
-        header: 'Pattern Party',
-        img: './imgs/ppsnip.png',
-        info: 'Pattern Party is a fun quick finger, memory game you can challenge friends with to see who can be the Partern Party Champion 🏆. This Game is a desktop application made with electron.</p>\
-        '
-    },
     lp: {
         header: 'Landing Page',
         img: './imgs/lpsnip.PNG',
-         link: 'https://m9-gbr.github.io/Uncle-Joe-s-Pizza/',
+        link: 'https://m9-gbr.io/Uncle-Joe-s-Pizza/',
         info: 'A landing page made for a fictional pizza company called Uncle Joe\'s Pizza 🍕'
     },
     xo: {
         header: 'X and O',
         img: './imgs/xando.PNG',
         link: 'https://m9-gbr.github.io/xando/',
-        info: 'A simple X and O web game made wih Javascript that has the ability to play with a friend or with the Computer.'
+        info: 'An X and O web game made wih Javascript with drawing animation and computer opponent functionality.'
     },
     cookie: {
         header: 'Cookie Library',
         img: './imgs/cookie.png',
         link: 'https://github.com/M9-GBR/cookie-library',
         info: 'A Library for easy creating, deletion and modification of a domain\'s cookies.'
+    },
+    forms: {
+        header: 'Forms Project',
+        img: './imgs/fpsnip.PNG',
+        link: 'https://m9-gbr.github.io/forms-project/sign-in.html',
+        info: 'An HTML forms project that includes a Sign In and Log In page.'
     }
 }
 
