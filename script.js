64e7af9a993a04915b864c79ec94410629be7f90
M9-GBR <150633267+M9-GBR@users.noreply.github.com>
2024-11-24 23:18:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -22,23 +22,35 @@ let data = {
         img: './imgs/fpsnip.PNG',
         link: 'https://m9-gbr.github.io/forms-project/sign-in.html',
         info: 'An HTML forms project that includes a Sign In and Log In page.'
+    },
+    clock: {
+        header: 'HTML Clock',
+        img: './imgs/clock.PNG',
+        link: 'https://m9-gbr.github.io/html-clock',
+        info: 'A clock built using HTML and the Canvas API.'
+    },
+    calculator: {
+        header: 'Calculator',
+        img: './imgs/calc.PNG',
+        link: 'https://m9-gbr.github.io/calculator',
+        info: 'A Calculator web app.'
     }
 }
 
 let prjDiv = document.getElementById('prj_sec_in')
 
 for (const prj in data) {
-    prjDiv.innerHTML += `<div class="card bg-dark text-bg-dark">\
-        <div class="card-header">\
-            <p class="h1">${data[prj].header}</p>\
-        </div>\
-        <div class="card-body">\
-            <img src="${data[prj].img}" class="card-img">\
-        </div>\
-        <div class="card-footer">\
-            <button type="button" class="btn btn-danger" data-bs-toggle="offcanvas"\
-                data-bs-target="#minfo" onclick="osbOpen('${prj}')">See More</button>\
-        </div>\
+    prjDiv.innerHTML += `<div class="card bg-dark text-bg-dark">
+        <div class="card-header">
+            <p class="h1">${data[prj].header}</p>
+        </div>
+        <div class="card-body">
+            <img src="${data[prj].img}" class="card-img">
+        </div>
+        <div class="card-footer">
+            <button type="button" class="btn btn-danger" data-bs-toggle="offcanvas"
+                data-bs-target="#minfo" onclick="osbOpen('${prj}')">See More</button>
+        </div>
     </div>`
 }
 
